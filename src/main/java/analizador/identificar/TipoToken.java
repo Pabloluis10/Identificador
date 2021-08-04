74d5de28373e51f375f0741d64007bc70694a9b0
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-08-04 10:01:50 -0600

    Arregle el enum TipoTiken

--- src/main/java/analizador/identificar/TipoToken.java
+++ src/main/java/analizador/identificar/TipoToken.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package analizador.identificar;
 
 /**
@@ -11,4 +6,15 @@ package analizador.identificar;
  */
 public enum TipoToken {
     
+    ID("Identificador"), ENTERO("Entero"), DECIMAL("Decimal"), SIMBOLO("Simbolo");
+    
+    private String tipo;
+    
+    private TipoToken(String tipo){
+        this.tipo = tipo;
+    }
+    
+    public String getTipo(){
+        return tipo;
+    }
 }
