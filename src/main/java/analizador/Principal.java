e4a89d921c5b97c2fd59f88f76b6b809006ba361
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-08-05 23:06:29 -0600

    En la clase principal cree un objeto GraficosAnalizador para iniciar el programa

--- src/main/java/analizador/Principal.java
+++ src/main/java/analizador/Principal.java
@@ -1,10 +1,15 @@
 
 package analizador;
-
+import analizador.interfazGrafica.GraficosAnalizador;
 /**
  *
  * @author pabloluis
  */
 public class Principal {
+    
+    public static void main(String[] args) {
+        GraficosAnalizador iniciar = new GraficosAnalizador();
+        iniciar.setVisible(true);
+    }
     
 }
